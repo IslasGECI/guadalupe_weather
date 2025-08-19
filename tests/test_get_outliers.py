5c603580733ec0a15892fb038dd476beca8a80b5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-19 20:37:24 +0000

    🛑🧪 Fail tests

--- tests/test_get_outliers.py
+++ tests/test_get_outliers.py
@@ -1,4 +1,4 @@
-from guadalupe_weather.get_outliers import get_outliers
+from guadalupe_weather.get_outliers import get_outliers, remove_outliers
 import pandas as pd
 
 outlier = 50
@@ -8,3 +8,8 @@ data = pd.DataFrame({"Variable": [2, 3, 5, 7, 9, 11, 14, 18, 22, outlier]})
 def test_get_outliers():
     obtained = get_outliers(data.Variable)
     assert obtained == outlier
+
+
+def tests_remove_outliers():
+    obtained = remove_outliers(data.Variable)
+    assert len(obtained) == len(data)
