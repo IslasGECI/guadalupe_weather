dfee5d6cf92ad2a17c02458ed5f2a9c3ad97c930
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-19 20:34:59 +0000

    ✅ Pass tests

--- guadalupe_weather/get_outliers.py
+++ guadalupe_weather/get_outliers.py
@@ -1,2 +1,12 @@
+import numpy as np
+
+
 def get_outliers(Variable):
-    pass
+    Q1 = np.percentile(Variable, 25)
+    Q3 = np.percentile(Variable, 75)
+    IQR = Q3 - Q1
+    linf = Q1 - 1.5 * IQR
+    lsup = Q3 + 1.5 * IQR
+
+    outliers = [x for x in Variable if x < linf or x > lsup]
+    return outliers[0]
