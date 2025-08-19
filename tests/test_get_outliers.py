19bec4b19611e4175a9684f0d98271f275e551b1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-19 20:43:32 +0000

    🛑🧪 Fail tests

--- tests/test_get_outliers.py
+++ tests/test_get_outliers.py
@@ -14,4 +14,4 @@ def test_get_outliers():
 def tests_remove_outliers():
     obtained = remove_outliers(data.Variable)
     assert len(obtained) == len(data)
-    assert obtained[-1] == np.nan
+    assert obtained.iloc[-1] == np.nan
