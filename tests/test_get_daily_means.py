19700fd46434cb7b8ffd809a38e2d7b861ec4981
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-20 18:37:52 +0000

    🛑🧪 Fail tests

--- tests/test_get_daily_means.py
+++ tests/test_get_daily_means.py
@@ -8,7 +8,11 @@ def test_get_daily_mean():
             "Date": ["30/Sep/2008", "30/Sep/2008", "01/Oct/2008", "01/Oct/2008", "01/Oct/2008"],
             "Time": ["03:00:00", "04:00:00", "11:00:00", "12:00:00", "13:00:00"],
             "Temp_Out": [19.2, 20.1, 21.5, 19.8, 18.7],
+            "Rain_Rate": [2, 2, 2, 2, 2],
         }
     )
-    obtained = get_daily_mean(data)
+    obtained = get_daily_mean(data, "Temp_Out")
     assert len(obtained) == 2
+    obtained = get_daily_mean(data, "Rain_Rate")
+    print(obtained)
+    assert obtained[0] == 2
