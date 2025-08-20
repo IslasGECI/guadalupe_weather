5f1ac6b3fb252776d972412653ce4facce883931
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-20 21:57:07 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,6 @@ runner = CliRunner()
 def test_version():
     result = runner.invoke(
         cli,
-        ["version"],
     )
     expected_version = "0.1.0"
     assert expected_version in result.stdout
