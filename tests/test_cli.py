7160ce2792b08b6728f2faa2f9fc0858f4824ce7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-20 21:52:58 +0000

    🛑🧪 Fail tests

---
+++ tests/test_cli.py
@@ -0,0 +1,13 @@
+from guadalupe_weather.cli import cli
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+def test_version():
+    result = runner.invoke(
+        cli,
+        ["version"],
+    )
+    expected_version = "0.1.0"
+    assert expected_version in result.stdout
