9e43c6503f071b693327173990f6b2b1e324f035
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-20 19:26:57 +0000

    🛑🧪 Fail tests

--- tests/test_get_outliers.py
+++ tests/test_get_outliers.py
@@ -37,5 +37,8 @@ def test_get_outliers():
 def tests_remove_outliers():
     column_name = "Variable"
     obtained = remove_outliers_for_column(data, column_name)
+
+    print(obtained)
     assert obtained.shape == data.shape
     assert np.isnan(obtained[column_name].iloc[-5])
+    assert np.isnan(obtained[column_name].iloc[-6])
