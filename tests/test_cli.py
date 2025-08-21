df5dcb45bac032b53059cdfaa7071700e8cd56c4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-21 16:44:14 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,9 +4,12 @@ from typer.testing import CliRunner
 runner = CliRunner()
 
 
+def test_remove_outliers():
+    result = runner.invoke(cli, ["remove-outliers"])
+    assert result.exit_code == 0
+
+
 def test_version():
-    result = runner.invoke(
-        cli,
-    )
+    result = runner.invoke(cli, ["version"])
     expected_version = "0.1.0"
     assert expected_version in result.stdout
