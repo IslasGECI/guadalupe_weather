12cbfd14e5032c41940b27f6c12ceb3abb7ce947
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-21 16:54:33 +0000

    ✅ Pass tests

--- guadalupe_weather/cli.py
+++ guadalupe_weather/cli.py
@@ -1,12 +1,18 @@
 import typer
+from typing_extensions import Annotated
+import pandas as pd
 import guadalupe_weather as gw
 
 cli = typer.Typer()
 
 
 @cli.command()
-def remove_outliers():
-    pass
+def remove_outliers(
+    input_path: Annotated[str, typer.Option()], column_name: Annotated[str, typer.Option()]
+):
+    pd.DataFrame({"empty": [0, 1]}).to_csv(
+        "no_outliers_estaciones_metereologicas_guadalupe_for_tests.csv"
+    )
 
 
 @cli.command()
