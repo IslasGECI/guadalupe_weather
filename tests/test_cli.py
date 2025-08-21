303aabb668b7b088d78391829e8e092a207f4b64
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-21 16:50:51 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,19 @@
 from guadalupe_weather.cli import cli
 from typer.testing import CliRunner
+import geci_test_tools as gtt
 
 runner = CliRunner()
 
 
 def test_remove_outliers():
-    result = runner.invoke(cli, ["remove-outliers"])
+    input_path = "tests/data/estaciones_metereologicas_guadalupe_for_tests.csv"
+    column_name = "Temp_Out"
+    result = runner.invoke(
+        cli, ["remove-outliers", "--input-path", input_path, "--column-name", column_name]
+    )
     assert result.exit_code == 0
+    expected_path = "no_outliers_estaciones_metereologicas_guadalupe_for_tests.csv"
+    gtt.assert_exist(expected_path)
 
 
 def test_version():
