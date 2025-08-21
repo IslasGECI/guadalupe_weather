74b9478a29dc61a2041e5674c997fe4f07b64ef5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-21 17:35:58 +0000

    ♻️ Refactor 🆙 upgrade minor number version

--- guadalupe_weather/__init__.py
+++ guadalupe_weather/__init__.py
@@ -1,5 +1,7 @@
 """Paquete analisis de clima en Isla Guadalupe"""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
+from .get_outliers import remove_outliers_for_column
+from .cli import cli
 from .plot_weather_variables import *  # noqa
 from .get_weather_data import *  # noqa
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -33,5 +33,5 @@ def test_remove_outliers():
 
 def test_version():
     result = runner.invoke(cli, ["version"])
-    expected_version = "0.1.0"
+    expected_version = "0.2.0"
     assert expected_version in result.stdout
