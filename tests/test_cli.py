6a7a8b9e9201304fc32f864f55cac832fc06e23b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-25 20:43:06 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,6 +15,30 @@ def test_remove_outliers():
     result = runner.invoke(
         cli,
         [
+            "remove-outliers",
+            "--input-path",
+            input_path,
+            "--column-name",
+            column_name,
+            "--output-path",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+    gtt.assert_exist(output_path)
+    obtained = pd.read_csv(output_path)
+    assert obtained.shape == (10, 39)
+    assert np.isnan(obtained.loc[9, "Temp_Out"])
+    assert "Temp_Out limits" in result.stdout
+
+
+def test_remove_outliers_for_column():
+    input_path = "tests/data/estaciones_meteorologicas_guadalupe_for_tests.csv"
+    column_name = "Temp_Out"
+    output_path = "tests/no_outliers_estaciones_meteorologicas_guadalupe_for_tests.csv"
+    result = runner.invoke(
+        cli,
+        [
             "remove-outliers-for-column",
             "--input-path",
             input_path,
