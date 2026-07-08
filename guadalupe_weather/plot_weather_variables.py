84caa9d89d9446e44ccaf2e82856c456fbcffb7d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-08 22:03:33 +0000

    ♻️ Refactor 🚧

--- guadalupe_weather/plot_weather_variables.py
+++ guadalupe_weather/plot_weather_variables.py
@@ -16,6 +16,51 @@ def plot_average_rain_by_zone(data_to_plot, box_plot_data, png_path, year_list):
     plt.savefig(png_path, dpi=300)
 
 
+def xxplot_average_and_boxplot_by_variable(data_to_plot, box_plot_data, variable, year_list):
+    config_by_variable = {
+        "temperature": {
+            "y_label": r"Temperature ($^{\circ}C$)",
+            "string_label": get_string_label_temperature,
+            "box_label": "Temperature typical year",
+            "y_lim_max": 30,
+        },
+        "rain": {
+            "y_label": "Monthly rainfall (mm/month)",
+            "string_label": get_string_label,
+            "box_label": "Rain typical year",
+            "y_lim_max": 50,
+        },
+    }
+    config = config_by_variable[variable]
+    fontsize = 20
+    ticks_positions = np.linspace(1, 12, 12)
+    months_labels = get_months_labels_list()
+    fig, ax = geci_plot()
+    box_plot = ax.boxplot(box_plot_data, patch_artist=True, boxprops=dict(facecolor="white"))
+    ax.plot(
+        data_to_plot.index,
+        data_to_plot.values,
+        "-o",
+        linewidth=2,
+        markeredgecolor="k",
+        markersize=5,
+        label=config["string_label"](year_list),
+    )
+    plt.xticks([*ticks_positions, 13], [*months_labels, ""], size=fontsize, rotation=90)
+    plt.yticks(size=fontsize)
+    plt.ylabel(config["y_label"], size=fontsize)
+    y_lim_min = -0.1
+    ax.set_ylim(
+        y_lim_min,
+        config["y_lim_max"],
+    )
+    handles, labels = ax.get_legend_handles_labels()
+    plt.legend(
+        [*handles, box_plot["boxes"].pop()], [*labels, config["box_label"]], fontsize=fontsize
+    )
+    plt.tight_layout()
+
+
 def plot_average_and_boxplot_by_variable(
     data_to_plot, box_plot_data, string_label, y_label, y_lim_max, box_label
 ):
