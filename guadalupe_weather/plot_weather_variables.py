71b83583148afdf9c9cc7d9d776ffc0f9ed13a7e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-08 21:52:44 +0000

    ♻️ Refactor

--- guadalupe_weather/plot_weather_variables.py
+++ guadalupe_weather/plot_weather_variables.py
@@ -51,35 +51,9 @@ def plot_average_temperature_by_zone(data_to_plot, box_plot_data, png_path, year
     y_label = r"Temperature ($^{\circ}C$)"
     box_label = "Temperature typical year"
     y_lim_max = 30
-    fontsize = 20
-    ticks_positions = np.linspace(1, 12, 12)
-    months_labels = get_months_labels_list()
-    fig, ax = geci_plot()
-    box_plot = ax.boxplot(box_plot_data, patch_artist=True, boxprops=dict(facecolor="white"))
-    ax.plot(
-        data_to_plot.index,
-        data_to_plot.values,
-        "-o",
-        linewidth=2,
-        markeredgecolor="k",
-        markersize=5,
-        label=string_label,
+    plot_average_and_boxplot_by_variable(
+        data_to_plot, box_plot_data, string_label, y_label, y_lim_max, box_label
     )
-    plt.xticks([*ticks_positions, 13], [*months_labels, ""], size=fontsize, rotation=90)
-    plt.yticks(size=fontsize)
-    plt.ylabel(y_label, size=fontsize)
-    y_lim_min = -0.1
-    ax.set_ylim(
-        y_lim_min,
-        y_lim_max,
-    )
-    handles, labels = ax.get_legend_handles_labels()
-    plt.legend(
-        [*handles, box_plot["boxes"].pop()],
-        [*labels, box_label],
-        fontsize=fontsize,
-    )
-    plt.tight_layout()
     plt.savefig(png_path, dpi=300)
 
 
