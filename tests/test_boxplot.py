984970fadf7247b2aa97d5a45b816dadcf907eb0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-09 21:17:21 +0000

    🛑🧪 Fail tests

--- tests/test_boxplot.py
+++ tests/test_boxplot.py
@@ -10,5 +10,8 @@ def test_plot_boxplot_typical_year():
     boxplot_data = [np.random.rand(np.random.randint(1, 10)) for _ in range(12)]
     obtained = plot_boxplot_typical_year(boxplot_data)
     plt.pyplot.savefig("tests/data/boxplot_typical_year.png", dpi=300)
-
     assert isinstance(obtained, plt.axes._axes.Axes)
+
+    obtained_last_xtick_text = obtained.get_xticklabels()[0].get_text()
+    expected_last_xtick_text = "January"
+    assert obtained_last_xtick_text == expected_last_xtick_text
