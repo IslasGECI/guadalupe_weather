b932b12078b93c5958d49c4235676425e768574f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-09 19:00:14 +0000

    ♻️ Refactor

--- guadalupe_weather/get_weather_data.py
+++ guadalupe_weather/get_weather_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 def get_multiannual_monthly_cumulative_rain(all_data_path, years_list):
     all_data_df = pd.read_csv(all_data_path)
     multiannual_data = [
-        xxget_monthly_cumulative_rain_by_year(all_data_df, year) for year in years_list
+        get_monthly_cumulative_rain_by_year(all_data_df, year) for year in years_list
     ]
     multiannual_average = pd.concat(multiannual_data, axis=1).agg("mean", 1)
     return multiannual_average.sort_index()
@@ -18,12 +18,7 @@ def get_multiannual_monthly_temperature(all_data_path, years_list):
     return multiannual_average.sort_index()
 
 
-def get_monthly_cumulative_rain_by_year(monthly_average_rain_path, year):
-    monthly_average_rain_df = pd.read_csv(monthly_average_rain_path)
-    return xxget_monthly_cumulative_rain_by_year(monthly_average_rain_df, year)
-
-
-def xxget_monthly_cumulative_rain_by_year(monthly_average_rain_df, year):
+def get_monthly_cumulative_rain_by_year(monthly_average_rain_df, year):
     data_by_year = get_data_by_year(monthly_average_rain_df, year)
     return get_monthly_average_cumulative_rain_by_zone(data_by_year)
 
