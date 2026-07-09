b95018b5f68162b0203fed28288f3dc0b3f74d33
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-09 15:59:51 +0000

    ♻️ Refactor

--- guadalupe_weather/plot_weather_variables.py
+++ guadalupe_weather/plot_weather_variables.py
@@ -11,6 +11,12 @@ def plot_average_rain_by_zone(data_to_plot, box_plot_data, png_path, year_list):
     plt.savefig(png_path, dpi=300)
 
 
+def plot_average_temperature_by_zone(data_to_plot, box_plot_data, png_path, year_list):
+    variable = "temperature"
+    xxplot_average_and_boxplot_by_variable(data_to_plot, box_plot_data, variable, year_list)
+    plt.savefig(png_path, dpi=300)
+
+
 def xxplot_average_and_boxplot_by_variable(data_to_plot, box_plot_data, variable, year_list):
     config_by_variable = {
         "temperature": {
@@ -54,42 +60,6 @@ def xxplot_average_and_boxplot_by_variable(data_to_plot, box_plot_data, variable
         [*handles, box_plot["boxes"].pop()], [*labels, config["box_label"]], fontsize=fontsize
     )
     plt.tight_layout()
-
-
-def plot_average_and_boxplot_by_variable(
-    data_to_plot, box_plot_data, string_label, y_label, y_lim_max, box_label
-):
-    fontsize = 20
-    ticks_positions = np.linspace(1, 12, 12)
-    months_labels = get_months_labels_list()
-    fig, ax = geci_plot()
-    box_plot = ax.boxplot(box_plot_data, patch_artist=True, boxprops=dict(facecolor="white"))
-    ax.plot(
-        data_to_plot.index,
-        data_to_plot.values,
-        "-o",
-        linewidth=2,
-        markeredgecolor="k",
-        markersize=5,
-        label=string_label,
-    )
-    plt.xticks([*ticks_positions, 13], [*months_labels, ""], size=fontsize, rotation=90)
-    plt.yticks(size=fontsize)
-    plt.ylabel(y_label, size=fontsize)
-    y_lim_min = -0.1
-    ax.set_ylim(
-        y_lim_min,
-        y_lim_max,
-    )
-    handles, labels = ax.get_legend_handles_labels()
-    plt.legend([*handles, box_plot["boxes"].pop()], [*labels, box_label], fontsize=fontsize)
-    plt.tight_layout()
-
-
-def plot_average_temperature_by_zone(data_to_plot, box_plot_data, png_path, year_list):
-    variable = "temperature"
-    xxplot_average_and_boxplot_by_variable(data_to_plot, box_plot_data, variable, year_list)
-    plt.savefig(png_path, dpi=300)
 
 
 def get_y_max_limit(all_years_data):
