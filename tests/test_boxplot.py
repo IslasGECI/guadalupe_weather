61b11da2870f636037e1bd8b80065e786c3cc83e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-09 21:02:44 +0000

    ♻️ Refactor

--- tests/test_boxplot.py
+++ tests/test_boxplot.py
@@ -2,14 +2,13 @@ from guadalupe_weather.plot_boxplot_typical_year import plot_boxplot_typical_yea
 from guadalupe_weather.get_weather_data import get_box_plot_data_temperature
 
 
-import pandas as pd
 import matplotlib as plt
+import numpy as np
 
 
 def test_plot_boxplot_typical_year():
-    monthly_average_rain_path = "tests/data/input_plot_average_rain_by_zone.csv"
-    monthly_average_rain_df = pd.read_csv(monthly_average_rain_path)
-    boxplot_data = get_box_plot_data_temperature(monthly_average_rain_df)
+    boxplot_data = [np.random.rand(np.random.randint(1, 10)) for _ in range(12)]
     obtained = plot_boxplot_typical_year(boxplot_data)
+    plt.pyplot.savefig("tests/data/boxplot_typical_year.png", dpi=300)
 
     assert isinstance(obtained, plt.axes._axes.Axes)
