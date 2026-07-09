b598404901c617d3e77e7b8033b923203794542e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-09 18:27:35 +0000

    🛑🧪 Fail tests

--- tests/test_plot_average_rain.py
+++ tests/test_plot_average_rain.py
@@ -144,7 +144,7 @@ def test_plot_average_rain_hash():
     png_path = "tests/data/cumulative_rain_norte_bosque_2017.png"
     year = [2017]
     monthly_average_rain_path = "tests/data/input_plot_average_rain_by_zone.csv"
-    data_to_plot = get_monthly_cumulative_rain_by_year(monthly_average_rain_path, year)
+    data_to_plot = get_multiannual_monthly_cumulative_rain(monthly_average_rain_path, year)
     monthly_average_rain_df = pd.read_csv(monthly_average_rain_path)
     box_plot_data = get_box_plot_data(monthly_average_rain_df)
     obtained = plot_average_rain_by_zone(data_to_plot, box_plot_data, png_path, year)
