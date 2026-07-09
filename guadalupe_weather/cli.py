268b2ffe5879d0fd73c9f8e8b5380cded46bffb4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-09 18:28:38 +0000

    ✅ Pass tests

--- guadalupe_weather/cli.py
+++ guadalupe_weather/cli.py
@@ -1,16 +1,18 @@
-import typer
-from typing_extensions import Annotated
-import pandas as pd
 from guadalupe_weather.get_outliers import (
     _remove_outliers,
     _remove_outliers_for_column,
 )
 from guadalupe_weather.get_weather_data import (
-    get_monthly_cumulative_rain_by_year,
+    get_multiannual_monthly_cumulative_rain,
     get_box_plot_data,
 )
 from guadalupe_weather.plot_weather_variables import plot_average_rain_by_zone
 import guadalupe_weather as gw
+
+import typer
+from typing_extensions import Annotated
+import pandas as pd
+from typing import List
 
 cli = typer.Typer()
 
@@ -18,11 +20,11 @@ cli = typer.Typer()
 @cli.command()
 def render_rain_across_year(
     input_path: Annotated[str, typer.Option()],
-    years: Annotated[int, typer.Option()],
+    years: Annotated[List[int], typer.Option()],
     output_path: Annotated[str, typer.Option()],
 ):
     monthly_average_rain_df = pd.read_csv(input_path)
-    data_to_plot = get_monthly_cumulative_rain_by_year(input_path, years)
+    data_to_plot = get_multiannual_monthly_cumulative_rain(input_path, years)
     box_plot_data = get_box_plot_data(monthly_average_rain_df)
     plot_average_rain_by_zone(data_to_plot, box_plot_data, output_path, years)
 
