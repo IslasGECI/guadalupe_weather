ac244f9b6481a23c77604323ca60a29f36ed6f39
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-09 19:13:14 +0000

    ♻️ Refactor

--- guadalupe_weather/plot_weather_variables.py
+++ guadalupe_weather/plot_weather_variables.py
@@ -65,6 +65,10 @@ def plot_average_and_boxplot_by_variable(data_to_plot, box_plot_data, variable, 
 
 def get_string_label_temperature(years):
     variable = "Temperature"
+    return get_string_label_for_variable(variable, years)
+
+
+def get_string_label_for_variable(variable, years):
     if len(years) > 1:
         return f"{variable} in {*years, }"
     return f"{variable} in {years[0]}"
