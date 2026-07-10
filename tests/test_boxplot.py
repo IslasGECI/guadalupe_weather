351db85591df8942f87d1be97b151250ab39ce8b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-10 21:09:17 +0000

    ♻️ Refactor

--- tests/test_boxplot.py
+++ tests/test_boxplot.py
@@ -7,10 +7,13 @@ import numpy as np
 
 def test_plot_boxplot_typical_year():
     boxplot_data = [np.random.rand(np.random.randint(1, 10)) for _ in range(12)]
+    expected_ylabel = "Y label (units)"
+    expected_box_label = "Box label"
+    expected_ylim = 1
     config = {
-        "y_label": r"Temperature ($^{\circ}C$)",
-        "box_label": "Temperature typical year",
-        "y_lim_max": 30,
+        "y_label": expected_ylabel,
+        "box_label": expected_box_label,
+        "y_lim_max": expected_ylim,
     }
     obtained = plot_boxplot_typical_year(boxplot_data, config)
     plt.pyplot.savefig("tests/data/boxplot_typical_year.png", dpi=300)
@@ -19,3 +22,13 @@ def test_plot_boxplot_typical_year():
     obtained_last_xtick_text = obtained.get_xticklabels()[0].get_text()
     expected_last_xtick_text = "January"
     assert obtained_last_xtick_text == expected_last_xtick_text
+
+    obtained_ylabel = obtained.get_ylabel()
+    assert obtained_ylabel == expected_ylabel
+
+    obtained_ylim = obtained.get_ylim()
+    assert obtained_ylim[1] == expected_ylim
+
+    obtained_legend_texts = [text.get_text() for text in obtained.get_legend().get_texts()]
+    expected_legend_texts = [expected_box_label]
+    assert obtained_legend_texts == expected_legend_texts
