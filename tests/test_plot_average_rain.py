ee804ec7da35d5678164a4eeeb67105414e9081c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-10 21:45:49 +0000

    ♻️ Refactor

--- tests/test_plot_average_rain.py
+++ tests/test_plot_average_rain.py
@@ -148,7 +148,8 @@ def test_plot_average_rain_hash():
     monthly_average_rain_path = "tests/data/input_plot_average_rain_by_zone.csv"
     monthly_average_rain_df = pd.read_csv(monthly_average_rain_path)
     data_to_plot = get_multiannual_monthly_cumulative_rain(monthly_average_rain_df, year)
-    box_plot_data = get_box_plot_data(monthly_average_rain_df)
+    variable = "Cumulative_rain"
+    box_plot_data = compute_monthly_boxplot_data_by_variable(monthly_average_rain_df, variable)
     obtained = plot_average_rain_by_zone(data_to_plot, box_plot_data, year)
 
     assert isinstance(obtained, plt.axes._axes.Axes)
