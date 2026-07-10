d8eb072fa89b86327c4f31f551279dcf80523884
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-10 21:42:32 +0000

    ♻️ Refactor

--- guadalupe_weather/cli.py
+++ guadalupe_weather/cli.py
@@ -8,7 +8,6 @@ from guadalupe_weather.get_weather_data import (
     get_multiannual_monthly_temperature,
     get_box_plot_data,
     get_box_plot_data_without_nan,
-    xxget_box_plot_data_without_nan,
 )
 from guadalupe_weather.plot_weather_variables import (
     plot_average_rain_by_zone,
@@ -37,7 +36,7 @@ def render_typical_year_boxplot(
             "y_label": r"Temperature ($^{\circ}C$)",
             "box_label": "Temperature typical year",
             "y_lim_max": 30,
-            "boxplot_method": xxget_box_plot_data_without_nan,
+            "boxplot_method": get_box_plot_data_without_nan,
         },
         "Cumulative_rain": {
             "y_label": "Monthly rainfall (mm/month)",
@@ -62,7 +61,7 @@ def render_temperature_across_year(
     monthly_average_df = pd.read_csv(input_path)
     variable = "Avg_Temp_Out"
     data_to_plot = get_multiannual_monthly_temperature(monthly_average_df, years)
-    box_plot_data = xxget_box_plot_data_without_nan(monthly_average_df, variable)
+    box_plot_data = get_box_plot_data_without_nan(monthly_average_df, variable)
     plot_average_temperature_by_zone(data_to_plot, box_plot_data, years)
     plt.savefig(output_path, dpi=300)
 
--- tests/test_plot_average_temperature.py
+++ tests/test_plot_average_temperature.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from guadalupe_weather.get_weather_data import (
     get_monthly_average_temperature_by_zone,
-    xxget_box_plot_data_without_nan,
+    get_box_plot_data_without_nan,
     get_multiannual_monthly_temperature,
 )
 
@@ -27,7 +27,7 @@ def test_get_box_plot_data_temperature():
     monthly_average_rain_path = "tests/data/input_plot_average_rain_by_zone.csv"
     monthly_average_rain_df = pd.read_csv(monthly_average_rain_path)
     variable = "Avg_Temp_Out"
-    obtained_box_plot_data = xxget_box_plot_data_without_nan(monthly_average_rain_df, variable)
+    obtained_box_plot_data = get_box_plot_data_without_nan(monthly_average_rain_df, variable)
     obtained_january_box_plot_data = obtained_box_plot_data[0]
     expected_january_box_plot_data = np.array([8.5, 14.5, 10.0])
     np.testing.assert_equal(obtained_january_box_plot_data, expected_january_box_plot_data)
